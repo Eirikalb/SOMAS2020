42dae71e5a723aaef9b26dd52b14eaafcf9ef552
Hardik Aggarwal <hardik.aggarwal17@imperial.ac.uk>
2021-01-05 21:10:34 +0530

    basic file

---
+++ internal/clients/team2/gifts_test.go
@@ -0,0 +1,13 @@
+package team2
+
+import (
+	"testing"
+)
+
+func CheckgiftRequest(t *testing.T) {
+	finaleval := {
+		{clientID: shared.Team1},
+		{clientID: shared.Team2},
+		{clientID: shared. Team3}
+	}
+}
